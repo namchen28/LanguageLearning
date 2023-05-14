98f09446fe2750ec870dd417607ba45f3d610028
namho <namnh.bi11-196@st.usth.edu.vn>
2023-05-14 22:45:10 +0700

    onboarding

---
+++ app/src/main/java/com/example/languagelearning/ViewPagerAdapter.java
@@ -0,0 +1,72 @@
+package com.example.languagelearning;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    Context context;
+
+    int sliderAllImages[] = {
+            R.drawable.screen1,
+            R.drawable.screen2,
+            R.drawable.screen3,
+    };
+
+    int sliderAllTitle[] = {
+            R.string.screen1,
+            R.string.screen2,
+            R.string.screen3,
+    };
+
+    int sliderAllDesc[] = {
+            R.string.screen1des,
+            R.string.screen2des,
+            R.string.screen3des,
+    };
+
+    public ViewPagerAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return sliderAllTitle.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slider_screen,container,false);
+
+        ImageView sliderImage = (ImageView) view.findViewById(R.id.sliderImage);
+        TextView sliderTitle = (TextView) view.findViewById(R.id.sliderTitle);
+        TextView sliderDesc = (TextView) view.findViewById(R.id.sliderDesc);
+
+        sliderImage.setImageResource(sliderAllImages[position]);
+        sliderTitle.setText(this.sliderAllTitle[position]);
+        sliderDesc.setText(this.sliderAllDesc[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
